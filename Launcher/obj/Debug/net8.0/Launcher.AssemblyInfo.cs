8279f7cc76d3ba1d96d62835a3e71355a59cdeef
NikasWolf <NikasWolf@yandex.ru>
2026-06-01 09:14:08 +0700

    test push

--- Launcher/obj/Debug/net8.0/Launcher.AssemblyInfo.cs
+++ Launcher/obj/Debug/net8.0/Launcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd92d5030e4bec9acc6237b6e705c2ff78cbf196")]
 [assembly: System.Reflection.AssemblyProductAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
