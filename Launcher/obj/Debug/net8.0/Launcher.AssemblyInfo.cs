45eccfe8235f2d77d1515e3f097786128791f974
NikasWolf <NikasWolf@yandex.ru>
2026-06-06 13:41:21 +0700

    после компиляции

--- Launcher/obj/Debug/net8.0/Launcher.AssemblyInfo.cs
+++ Launcher/obj/Debug/net8.0/Launcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+584232070dac6df4f5202df89572c44063af00a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d93749f6d4d81882fdbebe31d8caf9d28ac1d6d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
