1a352724f539257c6c0c0d52bdbe0a3765e63b41
NikasWolf <NikasWolf@yandex.ru>
2026-06-07 20:43:32 +0700

    Релиз

--- Launcher/obj/Debug/net8.0/Launcher.AssemblyInfo.cs
+++ Launcher/obj/Debug/net8.0/Launcher.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c838446d5fc0e392039322c8f0f7ceda6b2890")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937683bd1fba75b91c7442905ee0290ce118571b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Launcher/obj/Release/net8.0/win-x64/Launcher.AssemblyInfo.cs
+++ Launcher/obj/Release/net8.0/win-x64/Launcher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c554c811ae8beb4ffd06e30427f3aa0eaa45d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+937683bd1fba75b91c7442905ee0290ce118571b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Launcher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
